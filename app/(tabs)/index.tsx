cfaca07ca5ff1a2ace79110121f75490b50a837e
Alivi <hakeemalivi5@gmail.com>
2026-04-24 16:41:07 +0500

    feat: refactor ListScreen to use FlatList for improved rendering of pending items

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
 import CompletedItems from "@/components/list/CompletedItems";
-import ListHeroCard from "@/components/list/ListHeroCard";
 import PendingItemCard from "@/components/list/PendingItemCard";
-import TabScreenBackground from "@/components/TabScreenBackground";
 import { useGroceryStore } from "@/store/grocery-store";
 import { useClerk, useUser } from "@clerk/expo";
 import React, { useEffect } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { FlatList, View } from "react-native";
 
 const ListScreen = () => {
   const { user } = useUser();
@@ -18,26 +16,14 @@ const ListScreen = () => {
   }, []);
   const pendingItems = items?.filter((item) => !item.isPurchased);
   return (
-    <ScrollView
-      className="flex-1 bg-background py-4"
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingHorizontal: 20, gap: 14 }}
-    >
-      <TabScreenBackground />
-      <ListHeroCard />
-      <View className="flex-row items-center justify-between px-1">
-        <Text className="text-sm font-semibold uppercase tracking-[1px] text-muted-foreground">
-          Shopping items
-        </Text>
-        <Text className="text-sm text-muted-foreground">
-          {pendingItems.length} active
-        </Text>
-      </View>
-      {pendingItems.map((item) => (
-        <PendingItemCard key={item.id} item={item} />
-      ))}
-      <CompletedItems />
-    </ScrollView>
+    <FlatList
+      data={pendingItems}
+      keyExtractor={(item) => String(item.id)}
+      renderItem={({ item }) => <PendingItemCard item={item} />}
+      contentContainerStyle={{ padding: 20, gap: 14 }}
+      ListHeaderComponent={<View style={{ padding: 20, gap: 14 }}></View>}
+      ListFooterComponent={<CompletedItems />}
+    />
   );
 };
 
